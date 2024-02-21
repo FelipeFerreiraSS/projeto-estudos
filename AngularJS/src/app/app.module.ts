154c2e4a342bed002a569bd752cc46d3129b40f4
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2024-02-21 13:57:21 -0300

    Renderização condicional

--- AngularJS/src/app/app.module.ts
+++ AngularJS/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { FistComponentComponent } from './components/fist-component/fist-component.component';
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
 import { DirectivesComponent } from './components/directives/directives.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
 
 @NgModule({
-  declarations: [AppComponent, FistComponentComponent, ParentDataComponent, DirectivesComponent],
+  declarations: [AppComponent, FistComponentComponent, ParentDataComponent, DirectivesComponent, IfRenderComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
