7324ee1d1022fbbd73e5a33f89bb94c9f2913127
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2024-02-21 13:42:50 -0300

    diretivas

--- AngularJS/src/app/app.module.ts
+++ AngularJS/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FistComponentComponent } from './components/fist-component/fist-component.component';
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
+import { DirectivesComponent } from './components/directives/directives.component';
 
 @NgModule({
-  declarations: [AppComponent, FistComponentComponent, ParentDataComponent],
+  declarations: [AppComponent, FistComponentComponent, ParentDataComponent, DirectivesComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
