67fb89ae3790f8c3af5d6aa4f089350474827d33
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2024-02-21 11:46:47 -0300

    reset angular 13.1.2

--- AngularJS/src/app/app.component.spec.ts
+++ AngularJS/src/app/app.component.spec.ts
@@ -4,7 +4,9 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'projeto-angular' title`, () => {
+  it(`should have as title 'teoria'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('projeto-angular');
+    expect(app.title).toEqual('teoria');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, projeto-angular');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('teoria app is running!');
   });
 });
