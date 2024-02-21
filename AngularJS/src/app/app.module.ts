a5365115883ce3a7ab54d9c345ffad426e461d9a
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2024-02-21 13:24:49 -0300

    compartilhando dados

--- AngularJS/src/app/app.module.ts
+++ AngularJS/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FistComponentComponent } from './components/fist-component/fist-component.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
 
 @NgModule({
-  declarations: [AppComponent, FistComponentComponent],
+  declarations: [AppComponent, FistComponentComponent, ParentDataComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
