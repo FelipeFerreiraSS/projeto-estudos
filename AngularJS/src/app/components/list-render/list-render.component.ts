8b87654690456686571774892b38351b8dbbaa2b
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2024-02-22 10:09:09 -0300

    interfaces no Angular

--- AngularJS/src/app/components/list-render/list-render.component.ts
+++ AngularJS/src/app/components/list-render/list-render.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Animal } from 'src/app/Animal';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.css']
 })
 export class ListRenderComponent implements OnInit {
-  animals = [
-    { name:'Bob', type: 'cachorro' },
-    { name:'Tom', type: 'gato' },
-    { name: 'Rex', type: 'cachorro' }
+  animals: Animal[] = [
+    { name:'Bob', type: 'cachorro', age: 12 },
+    { name:'Tom', type: 'gato', age: 5 },
+    { name: 'Rex', type: 'cachorro', age: 2 }
   ]
 
   constructor() { }
