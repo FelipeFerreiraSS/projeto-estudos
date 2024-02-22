d474ede93656c87230d1e72794a866715e33c3c2
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2024-02-22 09:35:40 -0300

    Renderização de lista

---
+++ AngularJS/src/app/components/list-render/list-render.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-render',
+  templateUrl: './list-render.component.html',
+  styleUrls: ['./list-render.component.css']
+})
+export class ListRenderComponent implements OnInit {
+  animals = [
+    { name:'Bob', type: 'cachorro' },
+    { name:'Tom', type: 'gato' },
+    { name: 'Rex', type: 'cachorro' }
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
