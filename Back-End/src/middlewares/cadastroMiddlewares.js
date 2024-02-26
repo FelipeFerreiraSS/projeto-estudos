0ad20bf38be4488d5a414fca9809b58666aae349
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2024-02-26 11:25:06 -0300

    middlewares

---
+++ Back-End/src/middlewares/cadastroMiddlewares.js
@@ -0,0 +1,17 @@
+const validateBody = (request, response, next) => {
+  const { body } = request
+
+  if (body.nome === undefined){
+    return response.status(400).json({ menssage: 'O campo "nome" é obrigatório!' })
+  }
+
+  if (body.nome === ''){
+    return response.status(400).json({ menssage: 'O campo "nome" não pode ser vazio!' })
+  }
+
+  next()
+}
+
+module.exports = {
+  validateBody
+}
--- Back-End/src/router.js
+++ Back-End/src/router.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const cadastroControllers = require('./controllers/cadastroControllers')
+const cadastroMiddlewares = require('./middlewares/cadastroMiddlewares')
 
 const router = express.Router()
 
 router.get('/cadastro', cadastroControllers.getAll)
-router.post('/cadastro', cadastroControllers.createCadastro)
+router.post('/cadastro', cadastroMiddlewares.validateBody, cadastroControllers.createCadastro)
 
 module.exports = router
