9f22f4003d0421e602ff91db0e8b608a18d8b4cf
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2024-03-21 13:16:23 -0300

    projeto redux

---
+++ react-redux/src/data/products.js
@@ -0,0 +1,60 @@
+const products = [
+  {
+    id: "6228fe63b7e6cb904bbe0165",
+    price: 3,
+    name: "Café",
+    imageUrl: 
+      "https://images.unsplash.com/photo-1514432324607-a09d9b4aefdd?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: "6228fec7b7e6cb904bbe016f",
+    name: "Chocolate",
+    price: 15,
+    imageUrl:
+      "https://images.unsplash.com/photo-1623660053975-cf75a8be0908?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    price: 40,
+    id: "6228ff71b7e6cb904bbe0175",
+    name: "Bolo",
+    imageUrl:
+      "https://images.unsplash.com/photo-1602351447937-745cb720612f?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Pão",
+    price: 2,
+    imageUrl:
+      "https://images.unsplash.com/photo-1589367920969-ab8e050bbb04?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    id: "6228ff93b7e6cb904bbe0177",
+  },
+  {
+    price: 30,
+    id: "6228fe80b7e6cb904bbe0168",
+    name: "Queijo",
+    imageUrl:
+      "https://images.unsplash.com/photo-1624806992066-5ffcf7ca186b?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    price: 5,
+    name: "Leite",
+    id: "6228ff31b7e6cb904bbe0172",
+    imageUrl:
+      "https://images.unsplash.com/photo-1550583724-b2692b85b150?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    price: 8,
+    imageUrl:
+      "https://images.unsplash.com/photo-1497034825429-c343d7c6a68f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    id: "62290014b7e6cb904bbe017c",
+    name: "Sorvete",
+  },
+  {
+    id: "6228ffa5b7e6cb904bbe017a",
+    name: "Pizza",
+    price: 80,
+    imageUrl:
+      "https://plus.unsplash.com/premium_photo-1673439304183-8840bd0dc1bf?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
+
+export default products;
